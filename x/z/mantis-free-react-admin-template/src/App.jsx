179c9ed1183d4ba127db9c943d33213c89d1a978
Viraj Mandlik <138781016+virajmandlik@users.noreply.github.com>
2024-11-14 02:11:42 +0000

    first

---
+++ x/z/mantis-free-react-admin-template/src/App.jsx
@@ -0,0 +1,18 @@
+import { RouterProvider } from 'react-router-dom';
+
+// project import
+import router from 'routes';
+import ThemeCustomization from 'themes';
+
+import ScrollTop from 'components/ScrollTop';
+
+
+export default function App() {
+  return (
+    <ThemeCustomization>
+      <ScrollTop>
+        <RouterProvider router={router} />
+      </ScrollTop>
+    </ThemeCustomization>
+  );
+}
